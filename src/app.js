0d3b0b35c72d8cb7fa7d5629529c990fbaafaf69
chenxiaomian <939429880@qq.com>
2018-04-04 17:03:38 +0800

    add mimetype

--- src/app.js
+++ src/app.js
@@ -2,24 +2,38 @@ const http = require('http');
 const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
+const handlebars = require('handlebars');
 const promisify = require('util').promisify;
+
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
 const conf = require('./config/config');
+const mime = require('./helper/mime');
+
+// 读取模板文件
+const tplPath = path.join(__dirname, 'template/dir.tpl');
+const source = fs.readFileSync(tplPath);
+const template = handlebars.compile(source.toString());
 
 const server = http.createServer((req, res) => {
     const filePath = path.join(conf.root, req.url);
-
     stat(filePath).then(stats=>{
         if(stats.isFile()){
+            const contentType = mime(filePath);
             res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/plain');
+            res.setHeader('Content-Type', contentType);
             fs.createReadStream(filePath).pipe(res);
         }else if(stats.isDirectory()){
             readdir(filePath).then(files =>{
                 res.statusCode = 200;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end(files.join(','));
+                res.setHeader('Content-Type', 'text/html');
+                var dir = path.relative(conf.root, filePath);
+                const data = {
+                    files,
+                    title: path.basename(filePath),
+                    dir: dir ? `/${dir}` : ''
+                };
+                res.end(template(data));
             });
         }
     }).catch(err => {
---
+++ src/template/dir.tpl
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{title}}</title>
+    <style>
+    body{margin:0;padding:0;}
+    a{display: block;padding:10px;}
+    </style>
+</head>
+<body>
+    {{#each files}}
+        <a href="{{../dir}}/{{this}}">{{this}}</a>
+    {{/each}}
+</body>
+</html>
